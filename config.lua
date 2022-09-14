e85cebfc5380316dad54beb8fee4c602fa26fb43
Codine <113228542+CodineDev@users.noreply.github.com>
2022-09-13 21:40:13 -0700

    Add descriptions for options, turn off debug mode!

--- config.lua
+++ config.lua
@@ -1,12 +1,12 @@
 Config = {}
-Config.SyphonDebug = true -- Used for Debugging.. duh..
-Config.SyphonFuelDecor = "_FUEL_LEVEL" -- Do not touch! (Default: "_FUEL_LEVEL")
+Config.SyphonDebug = false -- Used for Debugging.. duh..
+Config.SyphonFuelDecor = "_FUEL_LEVEL" -- Do not touch!!!!!! (Default: "_FUEL_LEVEL")
 Config.SyphonKitCap = 50 -- Maximum amount (in L) the syphon kit can fit!
 -- Police Stuff --
 Config.SyphonPoliceCallChance = 25 -- Math.Random(1, 100) Default: 25% 
 Config.SyphonDispatchSystem = "qb-default" -- Options: "ps-dispatch", "qb-dispatch", "qb-default" (just blips) or "custom" (Custom: you must configure yourself!)
 -- Anims --
-Config.StealAnimDict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@'
-Config.StealAnim = 'machinic_loop_mechandplayer'
-Config.RefuelAnimDict = 'weapon@w_sp_jerrycan'
-Config.RefuelAnim = 'fire'
+Config.StealAnimDict = 'anim@amb@clubhouse@tutorial@bkr_tut_ig3@' -- Dictionary for the Syphon Animation
+Config.StealAnim = 'machinic_loop_mechandplayer' -- The Actual Syphon Animation
+Config.RefuelAnimDict = 'weapon@w_sp_jerrycan' -- Dictionary for the Refuel Animation
+Config.RefuelAnim = 'fire' -- The Actual Refuel Animation
